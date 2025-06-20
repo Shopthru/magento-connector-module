8c9c1a096d4d9914e6973ed674a9632af10cf4dc
Luke Rodgers <luke.rodgers@gene.co.uk>
2025-06-20 09:07:03 +0100

    Ensure modified classes for PHP 8.4 compat still instantiate

--- Test/Integration/ModuleIsInstalledTest.php
+++ Test/Integration/ModuleIsInstalledTest.php
@@ -5,6 +5,11 @@ namespace Shopthru\Connector\Test\Integration;
 use Magento\Framework\Module\ModuleListInterface;
 use Magento\TestFramework\Helper\Bootstrap;
 use PHPUnit\Framework\TestCase;
+use Shopthru\Connector\Model\ImportLog;
+use Shopthru\Connector\Model\ImportOrderManagement;
+use Shopthru\Connector\Model\ImportProcessors\DirectOrderCreator;
+use Shopthru\Connector\Model\Payment\Method\Shopthru;
+use Shopthru\Connector\Model\ResourceModel\ImportLog\Grid\Collection;
 
 class ModuleIsInstalledTest extends TestCase
 {
@@ -12,5 +17,16 @@ class ModuleIsInstalledTest extends TestCase
     {
         $moduleNames = Bootstrap::getObjectManager()->get(ModuleListInterface::class)->getNames();
         $this->assertContains('Shopthru_Connector', $moduleNames);
+    }
+
+    public function testSomeClassesInstantiate()
+    {
+        $objectManager = Bootstrap::getObjectManager();
+
+        $this->assertInstanceOf(ImportLog::class, $objectManager->get(ImportLog::class));
+        $this->assertInstanceOf(ImportOrderManagement::class, $objectManager->get(ImportOrderManagement::class));
+        $this->assertInstanceOf(DirectOrderCreator::class, $objectManager->get(DirectOrderCreator::class));
+        $this->assertInstanceOf(Shopthru::class, $objectManager->get(Shopthru::class));
+        $this->assertInstanceOf(Collection::class, $objectManager->get(Collection::class));
     }
 }
